5b671171634de768ff2a7b483eafa3ecfd88aa7b
io <io@localhost.localdomain>
2018-05-03 01:04:31 +0300

    it works

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2
 import argparse
-import os
-f = open("base.txt","a")
+import os, sys
 
 class contact:
     def __init__(self, Name, Date, Phone):
@@ -13,40 +12,61 @@ class contact:
         print self.Name, self.Date, self.Phone
 
     def addcontact(self):
+        f = open("base.txt","a")
         f.write(self.Name + " " + str(self.Date) + " " + str(self.Phone) + "\n")
+        f.close()
+
+    def list_contact(self):
+        f = open("base.txt","r")
+        print f.read()
+        f.close()
+
 
 class list_all(argparse.Action):
     def __call__(self, parser, *args, **kwargs):
-        print "List of all"
+        rew = contact('fake', 'fake', 'fake')
+        rew.list_contact()
 
 
-def name_type(string):
-    qwe = string
-    print qwe
-    return qwe
+class superparser:
+
+    def name_type(self,string):
+        qwe = string
+        return qwe
+
+    def date_type(self,string):
+        qwe = string
+        return qwe
+
+    def phone_type(self,string):
+        qwe = string
+        return qwe
+
+    def create_parser(self):
+
+        parser = argparse.ArgumentParser()
+
+        subparsers = parser.add_subparsers(help='choose type of action')
+
+        parser_w = subparsers.add_parser("write")
+        parser_w.add_argument("name", metavar=('[name]'), type=self.name_type)
+        parser_w.add_argument("date", metavar=('[date]'), type=self.date_type)
+        parser_w.add_argument("phone", metavar=('[phone]'), type=self.phone_type)
+
+        parser_l = subparsers.add_parser("list")
+        parser_l.add_argument("list", action=list_all, nargs='?',  help="show all contacts")
+
+        return parser
 
 
-def date_type(string):
-    qwe = string
-    print qwe
-    return qwe
+    def __init__(self):
 
-def phone_type(string):
-    qwe = string
-    print qwe
-    return qwe
-
-parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers(help='choose type of action')
-parser_w = subparsers.add_parser("write")
-parser_w.add_argument("name", metavar=('[name]'), type=name_type)
-parser_w.add_argument("date", metavar=('[date]'), type=date_type)
-parser_w.add_argument("phone", metavar=('[phone]'), type=phone_type)
-
-parser_l = subparsers.add_parser("list")
-parser_l.add_argument("list", action=list_all, nargs='?',  help="show all contacts")
+        self.parser = self.create_parser()
+        self.argspace = self.parser.parse_args()
 
 
-somevar = parser.parse_args()
+ololo = superparser()
+if str(sys.argv[1]) == "write":
+    wert = contact(ololo.argspace.name, ololo.argspace.date, ololo.argspace.phone)
+    wert.addcontact()
 
-print somevar
