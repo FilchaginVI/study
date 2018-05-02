7ebe86c43100b9687dfcb6835edb7e71f8b90e92
io <io@localhost.localdomain>
2018-05-02 23:32:44 +0300

    subparsers!

--- test.py
+++ test.py
@@ -15,6 +15,10 @@ class contact:
     def addcontact(self):
         f.write(self.Name + " " + str(self.Date) + " " + str(self.Phone) + "\n")
 
+class list_all(argparse.Action):
+    def __call__(self, parser, *args, **kwargs):
+        print "List of all"
+
 
 def name_type(string):
     qwe = string
@@ -33,13 +37,14 @@ def phone_type(string):
     return qwe
 
 parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers(help='sfdsfsef')
+subparsers = parser.add_subparsers(help='choose type of action')
 parser_w = subparsers.add_parser("write")
 parser_w.add_argument("name", metavar=('[name]'), type=name_type)
 parser_w.add_argument("date", metavar=('[date]'), type=date_type)
 parser_w.add_argument("phone", metavar=('[phone]'), type=phone_type)
 
-parser.add_argument("-l" , action='store_const', const=True, default=False, help="show all contacts")
+parser_l = subparsers.add_parser("list")
+parser_l.add_argument("list", action=list_all, nargs='?',  help="show all contacts")
 
 
 somevar = parser.parse_args()
